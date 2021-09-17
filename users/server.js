6b560e8666acf59432c5abf75e36c345a7beabde
Kelsi Proulx <kelsiproulx1919@gmail.com>
2021-09-17 13:08:20 -0700

    Initial commit

---
+++ users/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { graphqlHTTP: expressGraphQL } = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('Listening on http://localhost:4000/graphql');
+})
